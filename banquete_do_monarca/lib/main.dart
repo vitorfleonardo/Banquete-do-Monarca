ec4fa5d39a66490a903bd70d4cc647bd19ea39aa
Douglas <dmmartins.dm28@gmail.com>
2023-01-23 19:32:54 -0300

    att com retirada de erros

--- banquete_do_monarca/lib/main.dart
+++ banquete_do_monarca/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:banquete_do_monarca/core/app_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   runApp(const MyApp());
 
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
 
-  FirebaseFirestore.instace
-      .collection('consumidor')
-      .doc()
-      .set({'cpf': '06706767131', 'points': '100'});
+  // FirebaseFirestore.instance
+  //     .collection('consumidor')
+  //     .doc()
+  //     .set({'cpf': '06706767131', 'points': '100'});
 }
