49f87e7be279951c4b8afdd3e95ccd77867f836c
LuizaMaluf <luizamaluf03@gmail.com>
2023-01-24 12:29:45 -0300

    versão 2 da página de mercado

--- banquete_do_monarca/lib/pages/MenusLaterais/mercado_page.dart
+++ banquete_do_monarca/lib/pages/MenusLaterais/mercado_page.dart
@@ -26,10 +26,11 @@ class _MercadoPageState extends State<MercadoPage> {
                 ),
               ),
             ),
-            Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Column(mainAxisAlignment: MainAxisAlignment.start, 
+            children: [
               Container(
                   margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.11,
+                    top: MediaQuery.of(context).size.height * 0.110,
                     left: MediaQuery.of(context).size.width * 0.100,
                   ),
                   width: MediaQuery.of(context).size.width * 0.95,
@@ -39,21 +40,36 @@ class _MercadoPageState extends State<MercadoPage> {
                     image: AssetImage("assets/imagens_mercado/background.png"),
                     fit: BoxFit.fill,
                   )),
-                  child: Row(children: [
+                  child: Column(children: [
                     GestureDetector(
                       child: Container(
                         margin: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.height * 0.27,
-                          left: MediaQuery.of(context).size.width * 0.52,
+                          top: MediaQuery.of(context).size.height * 0.1,
+                          left: MediaQuery.of(context).size.width * 0.05,
                         ),
-                        width: MediaQuery.of(context).size.width * 0.2,
-                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.225,
                         child: Image.asset(
                           "assets/imagens_mercado/balao-fala.png",
                           fit: BoxFit.fill,
                         ),
                       ),
                     ),
+                  GestureDetector(
+                    child: Container(
+                        margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.16,
+                          right: MediaQuery.of(context).size.width * 0.635,
+                        ),
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        child: Image.asset(
+                          "assets/imagens_mercado/mercador.png",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                  )
+                  
                   ]))
             ]),
             Column(
