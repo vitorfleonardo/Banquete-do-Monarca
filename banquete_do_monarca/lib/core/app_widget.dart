c1895aa8a92265c5885cc8ba8e5aadb05c98a9c5
cadu <cadumotaalves@gmail.com>
2023-01-27 13:47:43 -0300

    Firebase com sucesso uhu

--- banquete_do_monarca/lib/core/app_widget.dart
+++ banquete_do_monarca/lib/core/app_widget.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:banquete_do_monarca/core/app_colors.dart';
 import 'package:banquete_do_monarca/pages/MenusLaterais/home_page.dart';
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
--- banquete_do_monarca/lib/main.dart
+++ banquete_do_monarca/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:banquete_do_monarca/core/app_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 void main() async {
   runApp(const MyApp());
-
-  await Firebase.initializeApp();
-
-  FirebaseFirestore.instace
-      .collection('consumidor')
-      .doc()
-      .set({'cpf': '06706767131', 'points': '100'});
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance
+  .collection("cliente")
+  .doc()
+  .set({'cpf': '056.809.411.58', 'pontos': '170'});
 }
+
