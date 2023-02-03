2b17f2491eec44ea9508c80261cf982ff27f6577
LuizaMaluf <luizamaluf03@gmail.com>
2023-02-03 19:29:13 -0300

    versão 2 - página de pagamento

--- banquete_do_monarca/lib/pages/Pagamento/pagamento_page.dart
+++ banquete_do_monarca/lib/pages/Pagamento/pagamento_page.dart
@@ -23,27 +23,54 @@ class PagamentoPage extends StatefulWidget {
 class _PagamentoPageState extends State<PagamentoPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraint) {
-          return Stack(
-            children: <Widget>[
-              const BackgroundGeneral(),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 1.0,
-                height: MediaQuery.of(context).size.height * 0.14,
+    return Scaffold(body: LayoutBuilder(builder: (context, constraint) {
+      return Stack(
+        children: <Widget>[
+          const BackgroundGeneral(),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 1.0,
+            height: MediaQuery.of(context).size.height * 0.14,
+            child: const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 160, 155, 155),
+              ),
+              child: Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    "Pagamento",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 36, 35, 35),
+                      fontSize: 40,
+                      fontFamily: 'awesomeLathusca',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 0.08,
+                  left: MediaQuery.of(context).size.width * 0.25,
+                ),
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.1,
                 child: const DrawerHeader(
                   decoration: BoxDecoration(
                     color: Color.fromARGB(255, 247, 247, 247),
                   ),
                   child: Center(
                     child: FittedBox(
-                      fit: BoxFit.fitWidth,
+                      fit: BoxFit.fill,
                       child: Text(
-                        "Pagamento",
+                        "Escolha sua forma de pagamento",
                         style: TextStyle(
                           color: Color.fromARGB(255, 36, 35, 35),
-                          fontSize: 40,
+                          fontSize: 25,
                           fontFamily: 'awesomeLathusca',
                           fontWeight: FontWeight.bold,
                         ),
@@ -52,14 +79,40 @@ class _PagamentoPageState extends State<PagamentoPage> {
                   ),
                 ),
               ),
-              SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.66,
-                  width: double.infinity,
-                  child: Column()),
             ],
-          );
-        },
-      ),
-    );
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.width * 0.2),
+                child: Center(
+                    child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Image.asset(
+                    "assets/imagens_pagamento/pix.png",
+                  ),
+                )),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.width * 0.35),
+                child: Center(
+                    child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Image.asset(
+                    "assets/imagens_pagamento/cartao.png",
+                  ),
+                )),
+              ),
+            ],
+          ),
+        ],
+      );
+    }));
   }
 }
