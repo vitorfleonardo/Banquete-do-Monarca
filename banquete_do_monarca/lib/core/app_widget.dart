3c386747b23997e33ec9cdfd530cabfa6d2ec0c7
LuizaMaluf <luizamaluf03@gmail.com>
2023-02-09 21:36:31 -0300

    pág de agradecimento

--- banquete_do_monarca/lib/core/app_widget.dart
+++ banquete_do_monarca/lib/core/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:banquete_do_monarca/pages/Login/login_page.dart';
 import 'package:banquete_do_monarca/pages/MenusLaterais/bebidas_page.dart';
 import 'package:banquete_do_monarca/pages/MenusLaterais/cardapio_page.dart';
 import 'package:banquete_do_monarca/pages/MenusLaterais/mercado_page.dart';
+import 'package:banquete_do_monarca/pages/Pagamento/obrigado.dart';
 import 'package:banquete_do_monarca/pages/Pagamento/pagamento_page.dart';
 import 'package:banquete_do_monarca/repository/cart_model.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,7 @@ class _MyAppState extends State<MyApp> {
       child: const MaterialApp(
         title: 'Banquete do Monarca',
         debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+        home: HomePage(),
       ),
     );
   }
